2d5bdd9912037b02bcb603a2f428d4e06e8937c1
borland <john_overklok@yahoo.com>
2019-07-30 02:25:32 +0500

    js: adapted, hashes

--- scripts/cal.js
+++ scripts/cal.js
@@ -1,5 +1,12 @@
 const FADE_TIME = 200;
 
+const MOBILE_TO = 991.98; /* md */
+
+const CLASS_SCHEDULE = "blk-schedule";
+const CLASS_PAGECTRL_BACK = "pagectrl__back";
+
+const ID_EVENTCARD_M_WRAP = "eventcard-active";
+
 const EVENTCARD_CLASSES = {
     BLOCK: "eventcard",
     BTN_CLOSE: "eventcard__button-close"
@@ -8,6 +15,7 @@ const EVENTCARD_CLASSES = {
 const CAL_CLASSES = {
     BLOCK: "cal",
     EVENT: "cal__event",
+    EVENT_LINK: "cal__event__link",
 
     TABLE_HEAD: "cal__table-head",
     TABLE_WRAP: "cal__table_wrap",
@@ -27,103 +35,253 @@ const CAL_CLASSES = {
     BG: "cal__bg"
 };
 
+var G_EVENTCARD_ACTIVE = undefined;
+var G_IS_MOBILE = undefined;
+
 $(document).ready(function() {
-    initAllCals();
+    initEvents();
     initAllEventcards();
+    initBackClick();
+
+    G_IS_MOBILE = ($(window).width() < MOBILE_TO);
+
+    $(window).resize(onWindowResize);
+
+    checkHash();
 });
 
-function initAllCals() {
+/**
+ * Инциализировать календарь
+ */
+function initEvents() {
     $("." + EVENTCARD_CLASSES.BLOCK).hide();
 
-    var $events = $("." + CAL_CLASSES.EVENT);
+    var $events = $("." + CAL_CLASSES.EVENT_LINK);
 
-    $events.click(function(evt) {
-        /// Get current Event and parent Calendar
-
-        // ":first" is just to ensure if proper object has been selected
-        var $event = $(evt.target);
-
-        if (!$event.hasClass(CAL_CLASSES.EVENT)) {
-            $event = $event.parents("." + CAL_CLASSES.EVENT + ":first");
-        }
-
-        var $cal = $event.parents("." + CAL_CLASSES.BLOCK + ":first");
-
-        if (!$cal[0]) return;
-
-        /// Get Eventcard ID current Event links to and corresponding Eventcard
-
-        var eventcard_id = $event.data("eventcard-id");
-        var $eventcard = $cal.find("." + EVENTCARD_CLASSES.BLOCK + "[data-id=" + eventcard_id + "]");
-
-        console.log(eventcard_id, $eventcard, $event);
-
-        /// Get Column container for current Event
-
-        var $event_col = $event.parent("." + CAL_CLASSES.COLUMN);
-
-        if (!$eventcard[0] || !$event_col[0] || !$event_col[0]) return;
-
-        /// Get child index of current Column container
-
-        var col_idx = $event_col.index();
-
-        /// Get day (and its column) responsible to current Event in table heading
-
-        var $cal__col_day = $cal.find("." + CAL_CLASSES.COLUMN_DAY + ":eq(" + col_idx + ")");
-        var $col_day = $cal__col_day.find("." + CAL_CLASSES.DAY);
-
-        if (!$cal__col_day[0] || !$col_day[0]) return;
-
-        /// Check if current Event has "closed" or "cancelled" status
-
-        var is_closed = $event.hasClass(CAL_CLASSES.EVENT_CLOSED),
-            is_cancelled = $event.hasClass(CAL_CLASSES.EVENT_CANCELLED);
-
-        /// Apply the status to the heading of the table
-
-        if (is_closed) {
-            $cal__col_day.addClass(CAL_CLASSES.COL_CLOSED);
-        } else if (is_cancelled) {
-            $cal__col_day.addClass(CAL_CLASSES.COL_CANCELLED);
-        }
-
-        /// Hide Calendar, show Eventcard, add background to the heading of the column
-
-        $cal.find("." + CAL_CLASSES.TABLE_WRAP).fadeOut(FADE_TIME);
-
-        $eventcard.fadeIn(FADE_TIME);
-
-        $("<div class='" + CAL_CLASSES.BG + "'></div>")
-            .hide().css({top: "110%"})
-            .prependTo($col_day)
-            .fadeIn(FADE_TIME).animate({top: "0"});
-    });
+    $events.on('click', onEventClick);
 }
 
+/**
+ * Иницализировать все карточки событий
+ */
 function initAllEventcards() {
     $eventcard_close_btns = $("." + EVENTCARD_CLASSES.BTN_CLOSE);
 
-    $eventcard_close_btns.click(function (evt) {
+    $eventcard_close_btns.click(onEventcardCloseClick);
+}
+
+function initBackClick() {
+    $("." + CLASS_PAGECTRL_BACK).click(function (evt) {
         evt.preventDefault();
 
-        var $btn = $(evt.target);
+        closeActiveEventcard();
+    })
+}
 
-        // ":first" is just to ensure if proper object has been selected
-        var $eventcard = $btn.parents("." + EVENTCARD_CLASSES.BLOCK + ":first");
-        var $cal = $btn.parents("." + CAL_CLASSES.BLOCK + ":first");
+function onWindowResize(evt) {
+    var width = $(window).width();
 
-        $eventcard.fadeOut(FADE_TIME);
+    if (width >= MOBILE_TO) {
+        if (G_IS_MOBILE === true) {
+            G_IS_MOBILE = false;
+            rebuildActiveEventcard();
+        }
+    } else {
+        if (G_IS_MOBILE === false) {
+            G_IS_MOBILE = true;
+            rebuildActiveEventcard();
+        }
+    }
+}
 
-        // Remove all backgrounds of table's heading
-        $cal.find("." + CAL_CLASSES.BG).fadeOut(FADE_TIME, function() {
-            this.remove();
+function checkHash() {
+    if (window.location.hash.startsWith("#eventcard-")) {
+        var eventcard_id = window.location.hash.split("eventcard-")[1];
 
-            // Remove possible status classes for ALL elements which has it
-            $cal.find("." + CAL_CLASSES.COL_CLOSED).removeClass(CAL_CLASSES.COL_CLOSED);
-            $cal.find("." + CAL_CLASSES.COL_CANCELLED).removeClass(CAL_CLASSES.COL_CANCELLED);
-        });
+        var eventcard = getEventcardById(eventcard_id);
 
-        $cal.find("." + CAL_CLASSES.TABLE_WRAP).fadeIn(FADE_TIME);
+        openEventcard(eventcard);
+    }
+}
+
+function onEventClick(evt) {
+    evt.preventDefault();
+
+    /// Get current Event and parent Calendar
+
+    // ":first" is just to ensure if proper object has been selected
+    var $event = $(evt.target).parent("." + CAL_CLASSES.EVENT);
+
+    /// Get Eventcard ID current Event links to and corresponding Eventcard
+    var $eventcard = getEventcardByEvent($event);
+    if (!$eventcard) return;
+
+    openEventcard($eventcard, $event);
+}
+
+function onEventcardCloseClick(evt) {
+    evt.preventDefault();
+
+    var $btn = $(evt.target);
+
+    // ":first" is just to ensure if proper object has been selected
+    var $eventcard = $btn.parents("." + EVENTCARD_CLASSES.BLOCK + ":first");
+
+    closeEventcard($eventcard);
+}
+
+function openEventcard($eventcard, $event) {
+    if (!$event) {
+        $event = getEventByEventcard($eventcard);
+    }
+
+    /// Get Column container for current Event
+    var $event_col = $event.parent("." + CAL_CLASSES.COLUMN);
+    if (!$event_col[0]) return;
+
+    /// Get child index of current Column container
+    var col_idx = $event_col.index();
+
+    /// Get day (and its column) responsible to current Event in table heading
+    var $cal__col_day = $("." + CAL_CLASSES.COLUMN_DAY + ":eq(" + col_idx + ")");
+    var $col_day = $cal__col_day.find("." + CAL_CLASSES.DAY);
+    if (!$cal__col_day[0] || !$col_day[0]) return;
+
+    /// Check if current Event has "closed" or "cancelled" status
+    var is_closed = $event.hasClass(CAL_CLASSES.EVENT_CLOSED),
+        is_cancelled = $event.hasClass(CAL_CLASSES.EVENT_CANCELLED);
+
+    /// Apply the status to the heading of the table
+    if (is_closed) {
+        $cal__col_day.addClass(CAL_CLASSES.COL_CLOSED);
+    } else if (is_cancelled) {
+        $cal__col_day.addClass(CAL_CLASSES.COL_CANCELLED);
+    }
+
+    if (G_IS_MOBILE) {
+        openEventcardMobile($eventcard);
+    } else {
+        openEventcardFull($eventcard, $col_day);
+    }
+
+    G_EVENTCARD_ACTIVE = $eventcard;
+    window.location.hash = "eventcard-" + $eventcard.data("id");
+}
+
+function closeEventcard($eventcard) {
+    if (G_IS_MOBILE) {
+        closeEventcardMobile($eventcard);
+    } else {
+        closeEventcardFull($eventcard);
+    }
+
+    G_EVENTCARD_ACTIVE = undefined;
+    window.location.hash = " ";
+
+    scrollToElement($("." + CLASS_SCHEDULE));
+}
+
+function openEventcardFull($eventcard, $col_day) {
+    /// Hide Calendar, show Eventcard, add background to the heading of the column
+    $("." + CAL_CLASSES.TABLE_WRAP).fadeOut(FADE_TIME);
+
+    $eventcard.fadeIn(FADE_TIME);
+
+    $("<div class='" + CAL_CLASSES.BG + "'></div>")
+        .hide().css({top: "110%"})
+        .prependTo($col_day)
+        .fadeIn(FADE_TIME).animate({top: "0"});
+}
+
+function openEventcardMobile($eventcard) {
+    var $pc_back = $("." + CLASS_PAGECTRL_BACK);
+
+    $("." + CLASS_SCHEDULE).hide();
+    $pc_back.fadeIn(FADE_TIME);
+
+    /// Hide Calendar, show Eventcard, add background to the heading of the column
+    $("." + CAL_CLASSES.TABLE_WRAP).fadeOut(FADE_TIME);
+
+    $eventcard_wrap = $("#" + ID_EVENTCARD_M_WRAP);
+
+    $eventcard = $eventcard.clone();
+
+    $eventcard.find("." + EVENTCARD_CLASSES.BTN_CLOSE).click(onEventcardCloseClick);
+
+    $eventcard_wrap.append($eventcard);
+    $eventcard.fadeIn(FADE_TIME);
+
+    scrollToElement($pc_back);
+}
+
+function closeEventcardFull($eventcard) {
+    $("." + CLASS_SCHEDULE).show();
+
+    $eventcard.fadeOut(FADE_TIME);
+    $("." + CLASS_PAGECTRL_BACK).fadeOut(FADE_TIME);
+
+    // Remove all backgrounds of table's heading
+    $("." + CAL_CLASSES.BG).fadeOut(FADE_TIME, function() {
+        this.remove();
+
+        // Remove possible status classes for ALL elements which has it
+        $("." + CAL_CLASSES.COL_CLOSED).removeClass(CAL_CLASSES.COL_CLOSED);
+        $("." + CAL_CLASSES.COL_CANCELLED).removeClass(CAL_CLASSES.COL_CANCELLED);
     });
+
+    $("." + CAL_CLASSES.TABLE_WRAP).fadeIn(FADE_TIME);
+}
+
+function closeEventcardMobile($eventcard) {
+    $("#" + ID_EVENTCARD_M_WRAP).empty();
+
+    closeEventcardFull($eventcard);
+}
+
+function rebuildActiveEventcard() {
+    if (G_EVENTCARD_ACTIVE) {
+        var eventcard_id = G_EVENTCARD_ACTIVE.data("id");
+
+        if (G_IS_MOBILE) {
+            closeEventcardFull(G_EVENTCARD_ACTIVE);
+        } else {
+            closeEventcardMobile(G_EVENTCARD_ACTIVE);
+        }
+
+        openEventcard(getEventcardById(eventcard_id));
+    }
+}
+
+function closeActiveEventcard() {
+    if (G_EVENTCARD_ACTIVE) {
+        closeEventcard(G_EVENTCARD_ACTIVE);
+    }
+}
+
+function getEventcardById(eventcard_id) {
+    var $eventcard = $("." + EVENTCARD_CLASSES.BLOCK + "[data-id=" + eventcard_id + "]");
+
+    if (!$eventcard[0]) return;
+
+    return $eventcard;
+}
+
+function getEventcardByEvent($event) {
+    var eventcard_id = $event.data("eventcard-id");
+
+    return getEventcardById(eventcard_id);
+}
+
+function getEventByEventcard($eventcard) {
+    var eventcard_id = $eventcard.data("id");
+
+    return $("." + CAL_CLASSES.EVENT + "[data-eventcard-id=" + eventcard_id + "]");
+}
+
+function scrollToElement($element) {
+    setTimeout(function() {
+        $("body,html").animate({
+            scrollTop: $element.offset().top
+        }, FADE_TIME);
+    }, FADE_TIME);
 }
