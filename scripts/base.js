85a025f69f66b6c0440ad59a0bdd4bc8bce7b832
borland <john_overklok@yahoo.com>
2019-08-06 00:32:28 +0500

    menu: js ios

--- scripts/base.js
+++ scripts/base.js
@@ -2,6 +2,21 @@
  * Базовый скрипт для динамических страниц
  */
 
+// ====== BOOTSTRAP GLOBALS ====== //
+const BS_COL_NUMBER = 12;
+
+const BS_SIZES_FROM = {
+    xs: 0,
+    sm: 576,
+    md: 768,
+    lg: 992,
+    xl: 1200
+};
+
+var BS_SIZE_CURRENT = undefined;
+
+// ====== HEADERNAV GLOBALS ====== //
+
 const HEADERNAV_CLASSES = {
     MAIN: "headernav",
     _COLLAPSED: "headernav_collapsed"
@@ -12,16 +27,39 @@ const HEADERSWITCH_CLASSES = {
     BURGER_OPENED: "burger_opened"
 };
 
-const BODY_NOSCROLL_CLASS = "noscroll";
+const BODY_NOSCROLL_CLASS = "noscroll-xs";
 
 $(document).ready(function() {
+    $(window).resize(onWindowResize);
+    onWindowResize();
+
     initHeadernavMenu();
 });
+
+function onWindowResize() {
+    var width = $(window).width();
+
+    $.each(BS_SIZES_FROM, function(key, value) {
+        if (width > value) BS_SIZE_CURRENT = key;
+    });
+
+    $(window).trigger("bs-change", [BS_SIZE_CURRENT]);
+}
 
 function initHeadernavMenu() {
     $("." + HEADERSWITCH_CLASSES.BURGER).click(function() {
         switchHeadernavMenu();
     });
+
+    initNoscrollForTouchscreen();
+}
+
+function initNoscrollForTouchscreen() {
+    document.body.addEventListener('touchmove', function(evt) {
+        if (BS_SIZE_CURRENT === "xs" && !isHeadernavMenuCollapsed()) {
+            evt.preventDefault();
+        }
+    }, { passive: false });
 }
 
 function switchHeadernavMenu() {
